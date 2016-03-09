4b2b0a57b17954747dbd2306010ec672283f5aad
Kyle Millns <kmillns@Kyle-Millnss-MacBook-Pro.local>
2016-03-09 00:10:31 -0600

    Make index date dynamic based on today and format photo template

--- app/index/route.js
+++ app/index/route.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findRecord('photo', '2015-02-01');
+    let today = new Date();
+    // ISO string returns something like '1970-01-01T00:00:00.000Z'
+    // Splitting on T gets us the date and time separated
+    // Grabbing 0 gets us date only, in the desired format
+    let todayString = today.toISOString().split('T')[0];
+
+    return this.store.findRecord('photo', todayString);
   }
 });
